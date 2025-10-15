3588028af7239a9de6a628135990626e2d3dfbe9
Dmitry <trushchev@yandex.ru>
2025-10-16 00:06:09 +0300

    Zadanie_1

---
+++ homework_1.py
@@ -0,0 +1,51 @@
+class Student:
+    def __init__(self, name, surname, gender):
+        self.name = name
+        self.surname = surname
+        self.gender = gender
+        self.finished_courses = []
+        self.courses_in_progress = []
+        self.grades = {}
+
+class Mentor:
+    def __init__(self, name, surname):
+        self.name = name
+        self.surname = surname
+        self.courses_attached = []
+
+    def rate_hw(self, student, course, grade):
+        if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
+            if course in student.grades:
+                student.grades[course] += [grade]
+            else:
+                student.grades[course] = [grade]
+        else:
+            return 'Ошибка'
+
+class Lecturer(Mentor):
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
+        self.grades = {}
+    def avg_grade(self):
+        if len(self.grades) > 0:
+            return sum(self.grades.values()) / len(self.grades)
+        else:
+            return 0
+
+class Reviewer(Mentor):
+    def rate_hw(self, student, course, grade):
+        if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
+            if course in student.grades:
+                student.grades[course] += [grade]
+            else:
+                student.grades[course] = [grade]
+        else:
+            return 'Ошибка'
+
+
+lecturer = Lecturer('Иван', 'Иванов')
+reviewer = Reviewer('Пётр', 'Петров')
+print(isinstance(lecturer, Mentor)) # True
+print(isinstance(reviewer, Mentor)) # True
+print(lecturer.courses_attached)    # []
+print(reviewer.courses_attached)    # []
